09a5b5fe5a78205d54e9ab8d83ec8f4f541cbadd
B0Dim <bodim@me.com>
2022-01-29 18:16:08 +0300

    create realm task

---
+++ RealmApp/Services/DataManager.swift
@@ -0,0 +1,49 @@
+//
+//  DataManager.swift
+//  RealmApp
+//
+//  Created by  BoDim on 29.01.2022.
+//
+
+import Foundation
+
+
+class DataManager {
+    static let shared = DataManager()
+    
+    private init() {}
+    
+    func createTempData(_ completion: @escaping() -> Void) {
+        if !UserDefaults.standard.bool(forKey: "done") {
+            UserDefaults.standard.set(true, forKey: "done")
+            
+            let shoppingList = TaskList()
+            shoppingList.name = "Shopping List"
+            
+            let moviesList = TaskList(value: [
+                "Movies List",
+                Date(),
+                [["Best film ever"],
+                 ["The best of the best", "Must have", Date(), true]]
+            ])
+            
+            let milk = Task()
+            milk.name = "Milk"
+            milk.note = "2L"
+            
+            let bread = Task(value: ["Bread", "", Date(), true])
+            let apples = Task(value: [
+                "name": "apples",
+                "note": "2kg"
+            ])
+            
+            shoppingList.tasks.append(milk)
+            shoppingList.tasks.insert(contentsOf: [bread, apples], at: 1)
+            
+            DispatchQueue.main.async {
+                StorageManager.shared.save([shoppingList, moviesList])
+                completion()
+            }
+        }
+    }
+}
---
+++ RealmApp/ViewControllers/TasksViewController.swift
@@ -0,0 +1,128 @@
+//
+//  TasksViewController.swift
+//  RealmApp
+//
+//  Created by  BoDim on 29.01.2022.
+//
+
+import RealmSwift
+
+class TasksViewController: UITableViewController {
+
+    var taskList: TaskList!
+    
+    private var currentTasks: Results<Task>!
+    private var completedTasks: Results<Task>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = taskList.name
+        currentTasks = taskList.tasks.filter("isComplete = false")
+        completedTasks = taskList.tasks.filter("isComplete = true")
+        
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonPressed)
+        )
+        navigationItem.rightBarButtonItems = [addButton, editButtonItem]
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        section == 0 ? currentTasks.count : completedTasks.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        section == 0 ? "CURRENT TASKS" : "COMPLETED TASKS"
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TasksCell", for: indexPath)
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = task.name
+        content.secondaryText = task.note
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    @objc private func addButtonPressed() {
+        showAlert()
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TasksViewController {
+    
+    private func showAlert() {
+        
+        let alert = AlertController.createAlert(withTitle: "New Task", andMessage: "What do you want to do?")
+        
+        alert.action { newValue, note in
+            self.saveTask(withName: newValue, andNote: note)
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func saveTask(withName name: String, andNote note: String) {
+        let task = Task(value: [name, note])
+        StorageManager.shared.save(task, to: taskList)
+        let rowIndex = IndexPath(row: currentTasks.index(of: task) ?? 0, section: 0)
+        tableView.insertRows(at: [rowIndex], with: .automatic)
+    }
+}
